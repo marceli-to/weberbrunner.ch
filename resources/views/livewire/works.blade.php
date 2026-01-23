2b98884da084a0af432058f12d963f5e7a0c3c90
Marcel Stadelmann <m@marceli.to>
2026-01-23 19:22:59 +0100

    wip

--- resources/views/livewire/works.blade.php
+++ resources/views/livewire/works.blade.php
@@ -5,9 +5,9 @@
     class="md:hidden fixed left-1/2 -translate-x-1/2 top-25 z-40" 
     :hasActiveFilters="$hasActiveFilters" />
 
-  <div class="md:col-span-3 lg:col-span-4 bg-green-50">
+  <div class="md:col-span-3 lg:col-span-4">
     <x-menu.filter.wrapper 
-      class="bg-red-50 px-20 py-20 md:pr-0 md:pl-20 lg:pl-40 w-3/4 h-dvh max-h-dvh fixed left-0 top-0 z-30 md:!block md:sticky md:top-14 lg:top-40 md:h-auto md:bg-transparent md:w-auto md:py-0"
+      class="bg-white px-20 py-20 md:pr-0 md:pl-20 lg:pl-40 w-3/4 h-full max-h-dvh fixed left-0 top-0 z-50 md:!block md:sticky md:top-14 lg:top-40 md:h-auto md:bg-transparent md:w-auto md:py-0"
       :types="$types"
       :status="$status"
       :locations="$locations"
