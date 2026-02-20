025ede2103f2099dc88348bbfb48ce54073eecdd
Marcel Stadelmann <m@marceli.to>
2026-02-20 08:41:21 +0100

    Extract reusable MediaPickerDrawer component with single/multi select support

--- resources/js/app/views/projects/web/MetaData.vue
+++ resources/js/app/views/projects/web/MetaData.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useProjectMeta } from '@/composables/useProjectMeta'
 import { useCollapsed } from '@/composables/useCollapsed'
@@ -10,11 +9,10 @@ import BackButton from '@/components/ui/BackButton.vue'
 import WebNavBar from '@/views/projects/components/navbar/Web.vue'
 import Card from '@/components/ui/Card.vue'
 import CollapsibleHeader from '@/components/ui/CollapsibleHeader.vue'
-import Drawer from '@/components/ui/drawer/Drawer.vue'
 import MediaCard from '@/components/media/MediaCard.vue'
 import AddButton from '@/components/media/AddButton.vue'
+import MediaPickerDrawer from '@/components/media/MediaPickerDrawer.vue'
 import Textarea from '@/components/ui/form/Textarea.vue'
-import RadioIcon from '@/components/icons/Radio.vue'
 import Button from '@/components/ui/form/Button.vue'
 
 const route = useRoute()
@@ -24,7 +22,6 @@ const {
 	saveDescription, saveOgImage, removeOgImage,
 } = useProjectMeta()
 const { collapsed, toggle } = useCollapsed('project-meta')
-const drawerView = ref('list')
 
 function goBack() {
 	router.push({ name: 'projects.show', params: { id: route.params.id } })
@@ -99,61 +96,12 @@ function goBack() {
 
     </Grid>
 
-    <Drawer
+    <MediaPickerDrawer
       :open="ogDrawerOpen"
-      :views="[{ label: 'Text / Bilder', value: 'list' }, { label: 'Bilder', value: 'grid' }]"
-      v-model:view="drawerView"
-      submit-label="Übernehmen"
-      cancel-label="Abbrechen"
+      :items="project.media"
+      v-model="selectedOgImage"
       @close="ogDrawerOpen = false"
-      @submit="saveOgImage">
-
-      <!-- List view -->
-      <Grid v-if="drawerView === 'list'" :cols="6" class="mt-40">
-        <Span class="col-span-4 col-start-2" v-for="item in project.media" :key="item.uuid">
-          <Grid :cols="4">
-            <Span class="col-span-3">
-              <button type="button" class="flex items-start gap-x-10 border-t-thin border-t-white pt-10 cursor-pointer w-full text-left" @click="selectedOgImage = item.uuid">
-                <RadioIcon
-                  :variant="selectedOgImage === item.uuid ? 'checked' : 'unchecked'"
-                  class="w-12 shrink-0 mt-2 text-white" />
-                <span class="text-white text-sm overflow-hidden text-ellipsis whitespace-nowrap">{{ item.original_name }}</span>
-              </button>
-            </Span>
-            <Span class="col-span-1">
-              <img
-                :src="item.thumbnail_url"
-                :alt="item.original_name"
-                class="w-full h-auto aspect-square object-cover bg-white cursor-pointer"
-                @click="selectedOgImage = item.uuid" />
-            </Span>
-          </Grid>
-        </Span>
-      </Grid>
-
-      <!-- Grid view -->
-      <Grid v-if="drawerView === 'grid'" :cols="12" class="mt-40">
-        <Span class="col-span-8 col-start-3">
-          <Grid :cols="3">
-            <button
-              v-for="item in project.media"
-              :key="item.uuid"
-              type="button"
-              class="cursor-pointer relative"
-              @click="selectedOgImage = item.uuid">
-              <img
-                :src="item.thumbnail_url"
-                :alt="item.original_name"
-                class="w-full h-auto aspect-square object-cover bg-white" />
-              <span class="absolute top-10 left-10">
-                <RadioIcon :variant="selectedOgImage === item.uuid ? 'checked' : 'unchecked'" class="w-12 text-white" />
-              </span>
-            </button>
-          </Grid>
-        </Span>
-      </Grid>
-
-    </Drawer>
+      @submit="saveOgImage" />
 
 	</template>
 
--- resources/js/app/views/projects/web/TeaserImage.vue
+++ resources/js/app/views/projects/web/TeaserImage.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useProjectTeaser } from '@/composables/useProjectTeaser'
 import PageTitle from '@/components/ui/PageTitle.vue'
@@ -8,10 +7,9 @@ import Span from '@/components/ui/grid/Span.vue'
 import BackButton from '@/components/ui/BackButton.vue'
 import WebNavBar from '@/views/projects/components/navbar/Web.vue'
 import SectionTitle from '@/components/ui/SectionTitle.vue'
-import Drawer from '@/components/ui/drawer/Drawer.vue'
 import MediaCard from '@/components/media/MediaCard.vue'
 import AddButton from '@/components/media/AddButton.vue'
-import RadioIcon from '@/components/icons/Radio.vue'
+import MediaPickerDrawer from '@/components/media/MediaPickerDrawer.vue'
 
 const route = useRoute()
 const router = useRouter()
@@ -19,7 +17,6 @@ const {
 	project, teaserImage, selectedTeaserImage, teaserDrawerOpen,
 	saveTeaserImage, removeTeaserImage,
 } = useProjectTeaser()
-const drawerView = ref('list')
 
 function goBack() {
 	router.push({ name: 'projects.show', params: { id: route.params.id } })
@@ -68,61 +65,12 @@ function goBack() {
 
     </Grid>
 
-    <Drawer
+    <MediaPickerDrawer
       :open="teaserDrawerOpen"
-      :views="[{ label: 'Text / Bilder', value: 'list' }, { label: 'Bilder', value: 'grid' }]"
-      v-model:view="drawerView"
-      submit-label="Übernehmen"
-      cancel-label="Abbrechen"
+      :items="project.media"
+      v-model="selectedTeaserImage"
       @close="teaserDrawerOpen = false"
-      @submit="saveTeaserImage">
-
-      <!-- List view -->
-      <Grid v-if="drawerView === 'list'" :cols="6" class="mt-40">
-        <Span class="col-span-4 col-start-2" v-for="item in project.media" :key="item.uuid">
-          <Grid :cols="4">
-            <Span class="col-span-3">
-              <button type="button" class="flex items-start gap-x-10 border-t-thin border-t-white pt-10 cursor-pointer w-full text-left" @click="selectedTeaserImage = item.uuid">
-                <RadioIcon
-                  :variant="selectedTeaserImage === item.uuid ? 'checked' : 'unchecked'"
-                  class="w-12 shrink-0 mt-2 text-white" />
-                <span class="text-white text-sm overflow-hidden text-ellipsis whitespace-nowrap">{{ item.original_name }}</span>
-              </button>
-            </Span>
-            <Span class="col-span-1">
-              <img
-                :src="item.thumbnail_url"
-                :alt="item.original_name"
-                class="w-full h-auto aspect-square object-cover bg-white cursor-pointer"
-                @click="selectedTeaserImage = item.uuid" />
-            </Span>
-          </Grid>
-        </Span>
-      </Grid>
-
-      <!-- Grid view -->
-      <Grid v-if="drawerView === 'grid'" :cols="12" class="mt-40">
-        <Span class="col-span-8 col-start-3">
-          <Grid :cols="3">
-            <button
-              v-for="item in project.media"
-              :key="item.uuid"
-              type="button"
-              class="cursor-pointer relative"
-              @click="selectedTeaserImage = item.uuid">
-              <img
-                :src="item.thumbnail_url"
-                :alt="item.original_name"
-                class="w-full h-auto aspect-square object-cover bg-white" />
-              <span class="absolute top-10 left-10">
-                <RadioIcon :variant="selectedTeaserImage === item.uuid ? 'checked' : 'unchecked'" class="w-12 text-white" />
-              </span>
-            </button>
-          </Grid>
-        </Span>
-      </Grid>
-
-    </Drawer>
+      @submit="saveTeaserImage" />
 
 	</template>
 
